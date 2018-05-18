7742f14a1b643a9e6a5e5efcfafd7ee99f525bd3
Guillermo <guille.gallo88@gmail.com>
2018-05-18 19:28:27 -0300

    second commit

--- src/Ejecutable.java
+++ src/Ejecutable.java
@@ -1,12 +1,9 @@
-
-
 public class Ejecutable {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Personaje pepe = new Personaje("Pepe", 100);
 		Personaje pipo = new Personaje("Pipo", 90);
-		
 		
 		pepe.pegar(pipo);
 		System.out.println(pipo.getEnergia());
@@ -14,5 +11,4 @@ public class Ejecutable {
 		pipo.pegar(pepe);
 		System.out.println(pepe.getEnergia());
 	}
-
 }
